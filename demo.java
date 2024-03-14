97049ab1b0e9998c217203c5f0f9a2b28011cc71
vimalavee <157955226+vimalavee@users.noreply.github.com>
2024-03-14 12:28:13 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("pushing code into git");
+		System.out.println("writing some content into the file");
 	}
 
 }
